9ef2e7d0c8700540bb8cea63a71514b7df9fe59c
marcokstephen <marcok.stephen@gmail.com>
2014-11-05 21:54:17 -0500

    Trying to revert to deprecated method

--- app/src/main/java/sm/com/httesting/MyActivity.java
+++ app/src/main/java/sm/com/httesting/MyActivity.java
@@ -1,24 +1,23 @@
 package sm.com.httesting;
 
+import android.app.ActionBar;
 import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.widget.DrawerLayout;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MyActivity extends FragmentActivity {
+public class MyActivity extends ActionBarActivity {
 
-    FragmentTransaction transaction;
     DrawerLayout mDrawerLayout;
     ListView mDrawerList;
+    ActionBar actionBar;
     public static String[] sectionTitles = {"All Categories","    Legal & Financial","    Health & Social",
             "    Recreation & Culture","    Family & Friends","    Spirituality & Wellbeing",
             "    Work & School","    Sex & Relationships","Suggest a Place","Favourites"};
@@ -27,31 +26,38 @@ public class MyActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+        actionBar = getActionBar();
 
-        transaction = getFragmentManager().beginTransaction();
+        //Setting the initial scene: the "All Categories" view
+        actionBar.setTitle(sectionTitles[0]);
         Fragment fragment = ResourceListFragment.newInstance(0);
-        transaction.replace(R.id.fragment_container,fragment);
-        transaction.addToBackStack(null);
-        transaction.commit();
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container,fragment)
+                .addToBackStack(null)
+                .commit();
 
+        //Assigning values for the drawerLayout and its corresponding ListView
         mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
         mDrawerList = (ListView)findViewById(R.id.left_drawer);
-
         mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, sectionTitles));
         mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                transaction = getFragmentManager().beginTransaction();
+                //We change the main fragment depending on which category the user picks to see
                 Fragment fragment = ResourceListFragment.newInstance(i);
-                transaction.replace(R.id.fragment_container,fragment);
-                transaction.addToBackStack(null);
-                transaction.commit();
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container,fragment)
+                        .addToBackStack(null)
+                        .commit();
 
                 //Changing the title of the action bar at the top of the app
                 //Accounting for the spaces that we added in the array elements
                 String newTitle = sectionTitles[i];
                 if (i>0&&i<8) newTitle = newTitle.substring(4);
-                getActionBar().setTitle(newTitle);
+                actionBar.setTitle(newTitle);
+
+                mDrawerList.setItemChecked(i, true);
+                mDrawerLayout.closeDrawer(mDrawerList);
             }
         });
     }

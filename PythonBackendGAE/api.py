ddee106e45fd112d8c4c9cd7d94447865e7dda59
kenliao94 <kenliao94@gmail.com>
2014-11-08 01:39:16 -0500

    add psedo code to api.py

--- PythonBackendGAE/api.py
+++ PythonBackendGAE/api.py
@@ -1,4 +1,6 @@
 import webapp2
+import cgi 
+import math
 
 class API (webapp2.RequestHandler):
    # implementing the API for the hackathon
@@ -12,16 +14,26 @@ class API (webapp2.RequestHandler):
 
 	cursor = db.cursor()
 
-	if(method == ""):
+	if(method == "AddUpVote"):
+	#Write a method for adding up vote 
+	#This acts on the Comment Table 
+	elif(method == "AddNewLocation"):
+	#Write a method for adding a new location
+	#This acts on the Location Table 
+
+	elif(method == "GetCategoryLocation"):
+	#Write a method for retrieving the location in a specific category 
+	#This method will cal the calculate_location(), add a dictionary for [distance] -> [location ID]
+	#Put distances into an array and sort the distance array. Use the distance to map back to location ID 
+
+	elif(method == "AddComment"):
+	#Write a method for adding a comment to a location 
+	#This acts on the Comment Table
 
 
-	elif(method == ""):
+   def calculate_location(lg1,la1,lg2,la2):
+	#Implement a method for calculate the sum of difference between Latitude and Longitude 
 
-
-	elif(method == ""):
-
-
-	elif(method == ""):
-
-	
+	sum = math.abs(lg1-lg2) + math.abs(la1-la2)
+	return sum  
 

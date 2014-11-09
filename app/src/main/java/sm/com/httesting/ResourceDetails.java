c19bdfd293cf4e12834ec9e485101387e2b62df2
marcokstephen <marcok.stephen@gmail.com>
2014-11-08 22:59:02 -0500

    Working on ResourceDetails layout

--- app/src/main/java/sm/com/httesting/ResourceDetails.java
+++ app/src/main/java/sm/com/httesting/ResourceDetails.java
@@ -1,20 +1,25 @@
 package sm.com.httesting;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 //This activity gives further details about
 //each listing returned by ThoughtSpot
 public class ResourceDetails extends Activity {
 
     Resource resourceObject;
+    private Context ctxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +29,7 @@ public class ResourceDetails extends Activity {
         String object = bundle.getString(ResourceListFragment.INTENT_RESOURCE_ARG);
         resourceObject = new Resource(object);
         setContentView(R.layout.activity_resource_details);
+        ctxt = this;
 
         Button buttonWeb = (Button) findViewById(R.id.button_website);
         Button buttonCall = (Button) findViewById(R.id.button_call);
@@ -67,6 +73,25 @@ public class ResourceDetails extends Activity {
                 startActivity(mapIntent);
             }
         });
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linear_layout_resource_details);
+        LayoutInflater lf = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        String[] commentsArray = {"This is the best place ever","this place sucks","this place is alright","this place gave me cancer and aids and I will never be the same","what you just said was one of the most insanely idiotic things i have ever heard, at no point in your rambling incoherent speech did you say anything that could resemble a rational thought. everyone in this room is now dumber for having listened to you. i award you no points, and may god have mercy on your soul","GET REKKED"};
+
+        for (int i = 0; i < commentsArray.length; i++){
+            View commentView = lf.inflate(R.layout.resource_comment_item,null);
+            TextView comment = (TextView)commentView.findViewById(R.id.textview_user_comment);
+            final Button helpfulButton = (Button)commentView.findViewById(R.id.button_mark_helpful);
+            helpfulButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(ctxt,"Helpful!",Toast.LENGTH_SHORT).show();
+                    helpfulButton.setEnabled(false);
+                }
+            });
+            comment.setText(commentsArray[i]);
+            linearLayout.addView(commentView);
+        }
     }
 
     @Override
